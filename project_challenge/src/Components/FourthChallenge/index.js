82eca7f5f5236cc4abed5c36dae803ed1c233439
Gabriel Wolf <65361452+GabrielWolf-Dev@users.noreply.github.com>
2021-07-31 18:52:57 -0300

    Update project finished

--- project_challenge/src/Components/FourthChallenge/index.js
+++ project_challenge/src/Components/FourthChallenge/index.js
@@ -7,17 +7,23 @@ export default function FourthChallenge() {
 
     function formCalc(e) {
         e.preventDefault();
-        const datas = new FormData(e.target);
-        const price = Number(datas.get('price'));
-        const tipPercent = Number(datas.get('tip'));
-        const peopleSplit = Number(datas.get('peopleSplit'));
+        const [priceString, tipPercentString, peopleSplitString] = document.querySelectorAll('.form-calc__input');
+        let price = Number(priceString.value);
+        const tipPercent = Number(tipPercentString.value);
+        const peopleSplit = Number(peopleSplitString.value);
         const tipValue = calcTip(price, tipPercent);
 
         setFormDatas({
             price: calcTotalPrice(price, tipValue),
-            tip: tipValue,
-            perPerson: calcPersonSplit(price, peopleSplit)
-        })
+            tip: tipValue.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'}),
+            perPerson: calcPersonSplit(price, peopleSplit),
+            peoples: peopleSplit
+        });
+
+        // Clear inputs
+        priceString.value = '';
+        tipPercentString.value = '';
+        peopleSplitString.value = '1';
     }
 
     function calcTip(price, percent) {
@@ -25,12 +31,12 @@ export default function FourthChallenge() {
     }
 
     function calcTotalPrice(price, tipPercent){
-        return Math.round(price + tipPercent);
-      }
+        return (price + tipPercent).toLocaleString('pt-br', {style: 'currency', currency: 'BRL'});
+    }
       
-      function calcPersonSplit(price, peopleSplit){
-        return price / peopleSplit;
-      }
+    function calcPersonSplit(price, peopleSplit){
+      return (price / peopleSplit).toLocaleString('pt-br', {style: 'currency', currency: 'BRL'});
+    }
 
     return(
         <>
@@ -41,15 +47,15 @@ export default function FourthChallenge() {
                 <form className="form-calc" onSubmit={formCalc}>
                     <fieldset className="form-calc-box">
                         <label className="form-calc__label" htmlFor="price">Valor da conta:</label>
-                        <input className="form-calc__input" required type="number" id="price" name="price" placeholder="Valor da conta" />
+                        <input className="form-calc__input" min="5" required type="number" id="price" name="price" placeholder="Valor da conta" />
                     </fieldset>
                     <fieldset className="form-calc-box">
-                        <label className="form-calc__label" htmlFor="tip">Gorjeta do garçom:</label>
+                        <label className="form-calc__label" htmlFor="tip">Gorjeta do garçom(%):</label>
                         <input className="form-calc__input" required type="number" id="tip" name="tip" placeholder="Gorjeta do garçom" />
                     </fieldset>
                     <fieldset className="form-calc-box">
                         <label className="form-calc__label" htmlFor="peopleSplit">Quantos dividem a conta?:</label>
-                        <input className="form-calc__input" required type="number" id="peopleSplit" name="peopleSplit" placeholder="Quantos dividem a conta?" />
+                        <input className="form-calc__input" min='1' required type="number" id="peopleSplit" name="peopleSplit" placeholder="Quantos dividem a conta?" />
                     </fieldset>
 
                     <input className="form-calc__sub" type="submit" value="Calcular" />
@@ -62,9 +68,19 @@ export default function FourthChallenge() {
                             ? <p style={{color: "var(--black)", marginTop: '8px'}} className="paragraph">Faça o cálculo para descobrir o resultado</p>
                             : (
                                 <ul className="result-form__list-results">
-                                    <li style={{color: "var(--black)", marginBottom: '8px'}} className="paragraph">{formDatas.price}</li>
-                                    <li style={{color: "var(--black)", margin: '8px 0'}} className="paragraph">{formDatas.tip}</li>
-                                    <li style={{color: "var(--black)", marginTop: '8px'}} className="paragraph">{formDatas.perPerson}</li>
+                                    <li style={{color: "var(--black)", marginBottom: '8px'}} className="paragraph">
+                                        {"Valor total: " + formDatas.price}
+                                    </li>
+                                    <li style={{color: "var(--black)", margin: '8px 0'}} className="paragraph">
+                                        {"Valor da gorjeta: " + formDatas.tip}
+                                    </li>
+                                    <li style={{color: "var(--black)", marginTop: '8px'}} className="paragraph">
+                                        {
+                                            formDatas.peoples > 1
+                                            ? "Cada pessoa vai pagar: " + formDatas.perPerson
+                                            : "Você vai pagar: " + formDatas.perPerson
+                                        }
+                                    </li>
                                 </ul>
                             )
                         }
